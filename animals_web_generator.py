1b7339a48b65c7483cb832018c541912020d5cb3
Lea <leaaimee2010@gmail.com>
2024-12-05 22:42:49 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,4 +1,7 @@
 import json
+import os
+print("Saving file to:", os.getcwd())
+
 
 def load_data():
     """ Loads a JSON file """
@@ -23,3 +26,40 @@ def get_animal_data(animals_data):
         print("\n")
 
 get_animal_data(animals_data)
+
+
+def generate_animal_html():
+    with open("animals_template.html", "r") as template_file:
+        html_template = template_file.read()
+    return html_template
+
+def generate_animal_info(animals_data):
+    animals_info = ""
+    for animal in animals_data:
+        name = animal.get("name", "unknown")
+        diet = animal.get("characteristics", {}).get("diet", "unknown")
+        location = ", ".join(animal.get("locations", ["unknown"]))
+        type = animal.get("characteristics", {}).get("type", "unknown")
+
+        animals_info += f"<li>Name: {name}<br>"
+        animals_info += f"Diet: {diet}<br>"
+        animals_info += f"Location: {location}<br>"
+        animals_info += f"Type: {type}</li>"
+
+    return animals_info
+
+
+def write_animal_html(animals_info):
+    """Replaces placeholder in the template and writes to a file"""
+
+
+    animals_info = generate_animal_info(animals_data)
+    html_template = generate_animal_html()
+    html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+
+    with open("animals.html", "w") as output_file:
+        output_file.write(html_output)
+        print("File written successfully to animals.html")
+
+get_animal_data(animals_data)
+write_animal_html(animals_data)
